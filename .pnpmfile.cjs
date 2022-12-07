c5b58295e43565280f5e15144d4beca2e993dcd4
Simon Bailey <simon@newtriks.com>
2022-12-07 11:16:56 +0000

    chore: upgrade to 61

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,20 +1,20 @@
 function readPackage(pkg, context) {
   const storybookViteDeps = {
     '@mdx-js/react': '^2.1.5',
-    '@storybook/addon-essentials': '^7.0.0-alpha.58',
-    '@storybook/addon-links': '^7.0.0-alpha.58',
-    '@storybook/addons': '^7.0.0-alpha.58',
-    '@storybook/addon-docs': '^7.0.0-alpha.58',
-    '@storybook/addon-interactions': '^7.0.0-alpha.58',
-    '@storybook/cli': '^7.0.0-alpha.58',
+    '@storybook/addon-essentials': '^7.0.0-alpha.61',
+    '@storybook/addon-links': '^7.0.0-alpha.61',
+    '@storybook/addons': '^7.0.0-alpha.61',
+    '@storybook/addon-docs': '^7.0.0-alpha.61',
+    '@storybook/addon-interactions': '^7.0.0-alpha.61',
+    '@storybook/cli': '^7.0.0-alpha.61',
     '@storybook/jest': '^0.0.10',
-    '@storybook/react': '^7.0.0-alpha.58',
-    '@storybook/react-vite': '^7.0.0-alpha.58',
+    '@storybook/react': '^7.0.0-alpha.61',
+    '@storybook/react-vite': '^7.0.0-alpha.61',
     '@storybook/test-runner': '^0.9.0',
     '@storybook/testing-library': '^0.0.13',
     globby: '^13.1.2',
     'http-proxy-middleware': '^2.0.6',
-    storybook: '^7.0.0-alpha.58',
+    storybook: '^7.0.0-alpha.61',
   };
 
   if (pkg.name === 'storybook-for-vite') {
