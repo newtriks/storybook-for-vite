b448424c5c8258c1ca97b33f4e65964b26d4d9f0
Simon Bailey <simon@newtriks.com>
2023-04-06 16:20:20 +0100

    chore: upgrade to latest SB release

--- .storybook/middleware.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-function expressMiddleWare(app) {
-  // Configure express instance to use proxy
-  app.use('/fact', createProxyMiddleware({ target: 'https://catfact.ninja', changeOrigin: true }));
-}
-
-module.exports = expressMiddleWare;
